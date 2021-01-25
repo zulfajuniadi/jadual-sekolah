52b5ea35b08f4d95fec960cce760d55b1e60131d
Zulfa Juniadi <zulfajuniadi@gmail.com>
2021-01-25 09:48:27 +0800

    Change jadual sekolah to jadualku

--- config/backpack/base.php
+++ config/backpack/base.php
@@ -24,7 +24,7 @@ return [
     // ----
 
     // Project name. Shown in the window title.
-    'project_name' => 'Jadual Sekolah',
+    'project_name' => 'JadualKu',
 
     // When clicking on the admin panel's top-left logo/name,
     // where should the user be redirected?
