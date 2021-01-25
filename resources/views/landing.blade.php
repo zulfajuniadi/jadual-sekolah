d18fb04ac244db46290ec793fbdffeed514546d8
Zulfa Juniadi <zulfajuniadi@gmail.com>
2021-01-25 10:27:56 +0800

    Add avatars

--- resources/views/landing.blade.php
+++ resources/views/landing.blade.php
@@ -1,4 +1,10 @@
 @extends('frontend')
+
+@php
+    $count = App\Models\Child::withoutGlobalScope(App\Models\Scopes\MyChildScope::class)->count();
+    $avatars = App\Models\Child::withoutGlobalScope(App\Models\Scopes\MyChildScope::class)->whereNotNull('avatar_config')->inRandomOrder()->take(6)->get();
+@endphp
+
 @section('content')
     <!-- Masthead -->
     <header class="masthead text-white text-center">
@@ -98,36 +104,6 @@
         </div>
     </section>
     
-    <!-- Testimonials -->
-    {{-- <section class="testimonials text-center bg-light d-none">
-        <div class="container">
-            <h2 class="mb-5">What people are saying...</h2>
-            <div class="row">
-                <div class="col-lg-4">
-                    <div class="testimonial-item mx-auto mb-5 mb-lg-0">
-                        <img class="img-fluid rounded-circle mb-3" src="img/testimonials-1.jpg" alt="">
-                        <h5>Margaret E.</h5>
-                        <p class="font-weight-light mb-0">"This is fantastic! Thanks so much guys!"</p>
-                    </div>
-                </div>
-                <div class="col-lg-4">
-                    <div class="testimonial-item mx-auto mb-5 mb-lg-0">
-                        <img class="img-fluid rounded-circle mb-3" src="img/testimonials-2.jpg" alt="">
-                        <h5>Fred S.</h5>
-                        <p class="font-weight-light mb-0">"Bootstrap is amazing. I've been using it to create lots of super nice landing pages."</p>
-                    </div>
-                </div>
-                <div class="col-lg-4">
-                    <div class="testimonial-item mx-auto mb-5 mb-lg-0">
-                        <img class="img-fluid rounded-circle mb-3" src="img/testimonials-3.jpg" alt="">
-                        <h5>Sarah W.</h5>
-                        <p class="font-weight-light mb-0">"Thanks so much for making these free resources available to us!"</p>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </section> --}}
-    
     <!-- Call to Action -->
     <section class="call-to-action text-white text-center">
         <div class="overlay"></div>
@@ -143,6 +119,22 @@
                         </div>
                     </div>
                 </div>
+            </div>
+        </div>
+    </section>
+    
+    <!-- Testimonials -->
+    <section class="testimonials text-center bg-light">
+        <div class="container">
+            <h2 class="mb-5 d-none">{{$count}} avatars and counting</h2>
+            <div class="row">
+                @foreach($avatars as $avatar)
+                <div class="col">
+                    <div class="testimonial-item mx-auto mb-5 mb-lg-0">
+                        <img width="120px" height="120px" class="img-fluid rounded-circle mb-3" src="/avatar/{{$avatar->id}}.svg" alt="">
+                    </div>
+                </div>
+                @endforeach
             </div>
         </div>
     </section>
