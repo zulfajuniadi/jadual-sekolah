a838a12e1da9c81dd097dc39b3b9fbefdf1681d2
Mior Muhammad Zaki <crynobone@gmail.com>
2021-02-07 09:47:36 +0800

    Add login tests.
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

---
+++ tests/Browser/LoginTest.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Tests\Browser;
+
+use App\Models\User;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Laravel\Dusk\Browser;
+use Tests\DuskTestCase;
+
+class LoginTest extends DuskTestCase
+{
+    /** @test */
+    public function it_can_login_a_valid_user()
+    {
+        $this->browse(function (Browser $browser) {
+            $browser->visit('/app/login')
+                ->waitForText('Log Masuk')
+                ->type('email', 'zulfajuniadi@gmail.com')
+                ->type('password', 'password')
+                ->press('Log Masuk')
+                ->waitForText('Jadual Hari')
+                ->assertPathIs('/app/dashboard')
+                ->assertAuthenticatedAs(User::find(1), backpack_guard_name());
+
+            $browser->logout(backpack_guard_name());
+        });
+    }
+
+    /** @test */
+    public function it_cannot_login_an_invalid_user()
+    {
+        $this->browse(function (Browser $browser) {
+            $browser->visit('/app/login')
+                ->waitForText('Log Masuk')
+                ->type('email', 'syafiq@gmail.com')
+                ->type('password', 'password')
+                ->press('Log Masuk')
+                ->waitFor('.invalid-feedback', 25)
+                ->assertSee('These credentials do not match our records.');
+        });
+    }
+}
